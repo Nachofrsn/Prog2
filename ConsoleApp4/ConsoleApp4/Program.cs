9b808470ca755a7c15c2f68ceb0e7892231e44f0
Nachofrsn <icordero@frsn.utn.edu.ar>
2023-08-15 11:22:36 -0300

    Clase 15/08, recursividad

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            string var;
+            num = int.Parse(Console.ReadLine());
+            Factorial(num, out int result);
+            Console.WriteLine("resultado: " + result);
+            Console.ReadKey();
+        }
+
+        static void Factorial(int num, out int result)
+        {
+            if (num >= 0) {
+                result = num;
+                for (int a = num - 1; a > 0; a--)
+                {
+                    result *= a;
+                }
+            }
+            else
+            {
+                Exception e = new Exception(" error en entero");
+                throw e;
+            } 
+
+        }
+    }
+}
+
+          
+
+            
+            
+            
+
---
+++ ConsoleApp4/ConsoleApp4/recursividad.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            num = int.Parse(Console.ReadLine());
+            int n = Facto(num);
+            Console.WriteLine("resultado: " + n);
+            Console.ReadKey();
+        }
+
+        /*
+        static void Factorial(int num, out int result)
+        {
+            if (num >= 0) {
+                result = num;
+                for (int a = num - 1; a > 0; a--)
+                {
+                    result *= a;
+                }
+            }
+            else
+            {
+                Exception e = new Exception("error en entero");
+                throw e;
+            } 
+
+        }
+        */
+
+        static int Facto(int num)
+        {
+            if (num > 1)
+            {
+                return num * Facto(num - 1);
+            }
+            else
+            {
+                return 1; 
+            }
+        }
+    }
+}
+
+
+
+          
+
+            
+            
+            
+
