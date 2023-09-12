505721ab8758da414d52986c2914a2e3f9b757e3
Nachofrsn <icordero@frsn.utn.edu.ar>
2023-09-12 11:27:25 -0300

    cambios

--- POO/Program.cs
+++ POO/Program.cs
@@ -9,13 +9,30 @@ namespace POO
     class Program
     {
         static void Main(string[] args)
-        {
+        {/*
             Persona A;
             A = new Persona();
             A.Nombre = "Pepe";
             A.Apellido = "Gomez";
             A.Apellido = " ";
-            Console.WriteLine(A.Apellido);
+            Console.WriteLine(A.Apelnom);
+
+            Politica B;
+            B = new Politica();
+            B.elecciones = "Aaafueraaa";
+            B.Casta = "casta";
+            Console.WriteLine(B.Afueraa);
+            */
+
+            Persona A, B;
+            A = new Persona();
+            B = new Persona("Rosas", "Andrea");
+
+            Console.WriteLine(A.Apelnom);
+            A.Apellido = "gomez";
+            A.Nombre = "lucas";
+            Console.WriteLine(A.Apelnom);
+            Console.WriteLine(B.Apelnom);
             Console.ReadKey();
         }
     }
