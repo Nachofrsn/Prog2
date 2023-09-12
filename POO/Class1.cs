0ef18e5d978289e300051323dd3989f1b6c7a515
Nachofrsn <icordero@frsn.utn.edu.ar>
2023-09-12 10:40:07 -0300

    Prog orientada a objetos 12/09

---
+++ POO/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO
+{
+    class Persona 
+    {
+        //propiedades
+        private string _Nombre;
+        private string _Apellido;
+
+        //Metodos de propiedad
+        public string Nombre
+        {
+            get { return this._Nombre;}
+            set { this._Nombre = value; }
+        }
+        public string Apellido
+        {
+            get { return this._Apellido; }
+            set
+            {
+                //trim() es para sacar espacios, es decir, deberia quedar todo texto
+                if (value.Trim() != string.Empty)
+                {
+                    this._Apellido = value;
+                }
+            }
+        }
+    }
+}
---
+++ POO/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona A;
+            A = new Persona();
+            A.Nombre = "Pepe";
+            A.Apellido = "Gomez";
+            A.Apellido = " ";
+            Console.WriteLine(A.Apellido);
+            Console.ReadKey();
+        }
+    }
+}
