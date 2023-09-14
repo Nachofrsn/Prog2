32715dee2e5e566bcde3e112b5442c745a883419
Nachofrsn <icordero@frsn.utn.edu.ar>
2023-09-14 11:34:47 -0300

    Nacho no para de hacer cagadas con git

---
+++ POO/POO/Class1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO
+{
+    class Persona 
+    {
+        //propiedades
+        private string _Nombre;
+        private string _Apellido;
+
+        //Metodos de propiedad
+        public Persona()
+        {
+            this.Apellido = "Perez";
+            this.Nombre = "Lujan";
+        }
+
+        public Persona(string AP, string NOM)
+        {
+            this.Apellido = AP;
+            this.Nombre = NOM;
+        }
+            
+        public string Nombre
+        {
+            get { return this._Nombre;}
+            set { this._Nombre = value; }
+        }
+
+        public string Apellido
+        {
+            get { return this._Apellido; }
+            set
+            {
+                //trim() es para sacar espacios, es decir, deberia quedar todo texto
+                if (value.Trim() != string.Empty)
+                {
+                    this._Apellido = value;
+                }
+            }
+        }
+
+        public string Apelnom
+        {
+            get { return this.Apellido + "," + this.Nombre; }
+        }
+    }
+
+    class Politica
+    {
+        private string _milei;
+        private string _massita;
+        private string _pato;
+
+
+        public string Casta
+        {
+            get { return this._massita + "," + this._pato; }
+            set
+            {
+                if (value.Trim() != string.Empty)
+                {
+                    this._massita = value;
+                    this._pato = value;
+                }
+            }
+        }
+
+        public string Afueraa
+        {
+            get { return this._milei; }
+        }
+
+        public string elecciones
+        {
+            get { return this._milei; }
+            set
+            {
+                if (value.Trim() != string.Empty)
+                {
+                    this._milei = value;
+                }
+            }
+        }
+    }
+    class ClaseCaja
+    {
+        private string P_CodigoInterno;
+        private string P_Contenido;
+        private float P_AltoCM;
+        private float P_LargoCM;
+        private float P_AnchoCM;
+        private float P_PesoKG;
+        private string P_Material;
+        public ClaseCaja()
+        {
+            //Constructor
+            this.CodigoInterno = "";
+            this.Contenido = "";
+            this.AltoCM = 0;
+            this.LargoCM = 0;
+            this.AnchoCM = 0;
+            this.PesoKG = 0;
+            this.Material = "";
+        }
+
+        public ClaseCaja(string N_CodigoInterno, string N_Contenido, float N_AltoCM, float N_LargoCM, float N_AnchoCM, float N_PesoKG, string N_Material)
+        {
+            //Constructor
+            this.CodigoInterno = N_CodigoInterno;
+            this.Contenido = N_Contenido;
+            this.AltoCM = N_AltoCM;
+            this.LargoCM = N_LargoCM;
+            this.AnchoCM = N_AnchoCM;
+            this.PesoKG = N_PesoKG;
+            this.Material = N_Material;
+        }
+        public ClaseCaja(float N_AltoCM, float N_PesoKG, string N_Contenido, string N_CodigoInterno)
+        {
+            this.AltoCM = N_AltoCM;
+            this.PesoKG = N_PesoKG;
+            this.Contenido = N_Contenido;
+            this.CodigoInterno = N_CodigoInterno;
+        }
+        public string CodigoInterno
+        {
+            get
+            {
+                return P_CodigoInterno;
+            }
+            set
+
+            {
+                P_CodigoInterno = value;
+
+            }
+
+        }
+        public string Contenido
+
+        {
+            get
+
+            {
+                return P_Contenido;
+
+            }
+            set
+
+            {
+                P_Contenido = value;
+            }
+
+        }
+        public float AltoCM
+
+        {
+            get
+
+            {
+                return P_AltoCM;
+
+            }
+            set
+
+            {
+                P_AltoCM = value;
+            }
+
+        }
+        public float AnchoCM
+        {
+            get
+            {
+                return P_AnchoCM;
+            }
+            set
+            {
+                P_AnchoCM = value;
+            }
+        }
+        public float LargoCM
+        {
+            get
+            {
+                return P_LargoCM;
+            }
+            set
+            {
+                P_LargoCM = value;
+            }
+        }
+        public float PesoKG
+        {
+            get
+            {
+                return P_PesoKG;
+            }
+            set
+            {
+                P_PesoKG = value;
+            }
+        }
+        public string Material
+        {
+            get
+            {
+                return P_Material;
+            }
+            set
+            {
+                P_Material = value;
+            }
+        }
+        public float VolumenCM3
+        {
+            //propiedad de solo lectura
+            get
+            {
+                return this.AltoCM * this.AnchoCM * this.LargoCM;
+            }
+        }
+        ~ClaseCaja()
+        {
+            //destructor
+        }
+    }
+}
---
+++ POO/POO/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {/*
+            Persona A;
+            A = new Persona();
+            A.Nombre = "Pepe";
+            A.Apellido = "Gomez";
+            A.Apellido = " ";
+            Console.WriteLine(A.Apelnom);
+
+            Politica B;
+            B = new Politica();
+            B.elecciones = "Aaafueraaa";
+            B.Casta = "casta";
+            Console.WriteLine(B.Afueraa);
+            
+
+            Persona A, B;
+            A = new Persona();
+            B = new Persona("Rosas", "Andrea");
+
+            Console.WriteLine(A.Apelnom);
+            A.Apellido = "gomez";
+            A.Nombre = "lucas";
+            Console.WriteLine(A.Apelnom);
+            Console.WriteLine(B.Apelnom);
+            */
+
+            ClaseCaja cajita;
+            cajita = new ClaseCaja();
+            cajita.AltoCM = 10;
+            cajita.AnchoCM = 10;
+            cajita.Material = "carton";
+            cajita.PesoKG = 20;
+            cajita.CodigoInterno = "0bjklm";
+            cajita.Contenido = "un lapiz";
+            cajita.LargoCM = 10;
+            Console.WriteLine(cajita.VolumenCM3);
+
+            List<ClaseCaja> deposito;
+            deposito = new List<ClaseCaja>();
+            ClaseCaja A = new ClaseCaja(10,20,"lapicera","Caja1");
+            ClaseCaja B = new ClaseCaja(20,10,"fibron","Caja2");
+            ClaseCaja C = new ClaseCaja(30,40,"lapiz","Caja3");
+            ClaseCaja D = new ClaseCaja(5,10,"goma","Caja4");
+            deposito.Add(A);
+            deposito.Add(B);
+            deposito.Add(C);
+            deposito.Add(D);
+            float peso = 0;
+            string cajapesada = "";
+            /*
+            for (int i = 0; i < deposito.Count(); i++)
+            {
+                Console.WriteLine($"{deposito[i].AltoCM}-{deposito[i].PesoKG}-{deposito[i].Contenido}-{deposito[i].CodigoInterno}");
+            }
+            */
+            foreach(ClaseCaja aux in deposito)
+            {
+                Console.WriteLine($"{aux.AltoCM}-{aux.PesoKG}-{aux.Contenido}-{aux.CodigoInterno}");
+                if (aux.PesoKG > peso)
+                {
+                    peso = aux.PesoKG;
+                    cajapesada = aux.CodigoInterno;
+                } 
+            }
+            Console.WriteLine($"{cajapesada}");
+            Console.ReadKey();
+        }
+    }
+}
