b5d473348db9c0cb40eb3d783c58660901847bfb
Nachofrsn <icordero@frsn.utn.edu.ar>
2023-09-28 21:28:57 -0300

    progreso en casa

--- POO/POO/ClaseFutbol.cs
+++ POO/POO/ClaseFutbol.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,14 +9,56 @@ namespace POO
 {
     class ClaseFutbol
     {
-        private List<ClaseFutbol> _equipos;
-        private string _jugadores;
-        private string _capitan;
+        private List<ClaseFutbol> _equiposFutbol;
+        private int _jugadores;
+        private string _nombre;
 
-        public ClaseFutbol(int numeroEquipo)
+        public ClaseFutbol(int jugadores, string nombre)
         {
-
+            this.Jugadores = jugadores;
+            this.Nombre = nombre;
+            this._equiposFutbol = new List<ClaseFutbol>();
         }
-
+        public int Jugadores
+        {
+            get { return _jugadores; }
+            set { _jugadores = value; }
+        }
+        public string Nombre
+        {
+            get { return _nombre; }
+            set { _nombre = value; }
+        }
+        public string Equipo
+        {
+            get { return _nombre + "," + _jugadores; }
+        }
+        public int CantidadEquipos
+        {
+           get 
+           {
+               int cantEquipos = 0;
+               foreach (ClaseFutbol equipoAux in _equiposFutbol)
+               {
+                    cantEquipos++;
+               } 
+               return cantEquipos;
+           }
+        }
+        public bool AgregarEquipo (ClaseFutbol nuevoEquipo)
+        {
+            this._equiposFutbol.Add(nuevoEquipo);
+            return true;
+        }
+        public bool AgregarCapitan (string equipo, string capitan)
+        {
+            for (int a = 0; a < CantidadEquipos; a++)
+            {
+                if (equipo == this._equiposFutbol[a].Nombre) 
+                { 
+                    
+                }
+            }
+        }
     }
 }
